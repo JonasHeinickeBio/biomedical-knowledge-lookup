4b87643b0446bcc7b1e818b56ed08d1d26528aa5
Jonas Heinicke <jonas.heinicke@helmholtz-hzi.de>
2026-04-24 11:57:28 +0200

    test: add pytestmark to tests/unit/test_disgenet_adapter.py

--- tests/unit/test_disgenet_adapter.py
+++ tests/unit/test_disgenet_adapter.py
@@ -4,8 +4,9 @@ Unit tests for DisGeNETAdapter.
 
 from unittest.mock import AsyncMock, patch
 
-import aiohttp
 import pytest
+
+pytestmark = pytest.mark.unit
 from knowledge_lookup.adapters.disgenet_adapter import DisGeNETAdapter
 from knowledge_lookup.models import KnowledgeSource, LookupConfig
 
